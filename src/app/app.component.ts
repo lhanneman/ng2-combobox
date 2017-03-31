f5fc215516a85c38d6bbc31fe37e591c60e4f708
lhanneman <lhanneman@uplandsoftware.com>
2017-03-31 12:14:01 -0500

    added ability to connect two dropdowns and have one filter the other; UI tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,135 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  optionsA = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 
-  'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'];
+  //  optionsA = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 
+  //  'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen'];
+  optionAValueProp = 'value';
+  optionALabelProp = 'label';
+  optionASelectedValue = '';
+  optionBSelectedValue = '';
+  optionCSelectedValue = '';
+
+
+  optionsA = [
+    {
+      'label': 'one',
+      'value': 1
+    },
+    {
+      'label': 'two this is a longer value lets see how it displays ',
+      'value': 2
+    },
+    {
+      'label': 'three',
+      'value': 3
+    },
+    {
+      'label': 'four',
+      'value': 4
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+    {
+      'label': 'five',
+      'value': 5
+    },
+  ];
 
   optionsB = ['oranges', 'apples', 'bananas'];
   optionsC = ['Ford', 'Chevrolet', 'Subaru', 'Toyota'];
+
+  separatorLabel = "name";
+  dividerLabel = 'name';
+  dividerFilterProp = 'separator';
+
+
+  separators = [
+    {
+      'id': 1,
+      'name': 'Ford',
+    },
+    {
+      'id': 3,
+      'name': 'Subaru',
+    },
+    {
+      'id': 6,
+      'name': 'Dodge',
+    },
+  ];
+
+  dividers = [
+    {
+      'id': 1,
+      'name': 'Focus',
+      'separator': 'Ford'
+    },
+    {
+      'id': 3,
+      'name': 'Legacy',
+      'separator': 'Subaru'
+    },
+    {
+      'id': 6,
+      'name': 'Charger',
+      'separator': 'Dodge'
+    },
+    {
+      'id': 6,
+      'name': 'Unknown (any sep)',
+      'separator': ''
+    },
+  ];
+
+  setOptionAValue(selectedOption) {
+    this.optionASelectedValue = selectedOption.label + " (" + selectedOption.value + ")";
+  }
+  setOptionBValue(selectedOption) {
+    this.optionBSelectedValue = selectedOption.label + " (" + selectedOption.value + ")";
+  }
+   setOptionCValue(selectedOption) {
+    this.optionCSelectedValue = selectedOption.label + " (" + selectedOption.value + ")";
+  }
 }
--- src/app/combobox/ng2-combobox.component.ts
+++ src/app/combobox/ng2-combobox.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, Output, AfterViewInit, ChangeDetectorRef, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ng2-combobox',
@@ -6,65 +6,92 @@ import { Component, Input, AfterViewInit, ChangeDetectorRef } from '@angular/cor
   styleUrls: ['./ng2-combobox.component.css']
 })
 export class Ng2ComboboxComponent implements AfterViewInit {
-    @Input() options: Array<string>;
-    @Input() manualEntry = false;
-    @Input() width: string = "200px";
-    @Input() autoFilter = false;
+  @Input() options: Array<string>;
+  @Input() manualEntry = false;
+  // @Input() width: string = "100%";
+  @Input() autoFilter = true;
+  @Input() valueProp: string;
+  @Input() labelProp: string;
+  @Input() connectTo: Ng2ComboboxComponent
+  @Input() connectToFilterProp: string;
 
-    selectedOption: string = "";
-    open = false;
-    filteredOptions: Array<string> = [];
-    currentHoverIndex = -1;
+  @Output() selectionChanged = new EventEmitter();
 
-    constructor(private cdr: ChangeDetectorRef) {}
+  selectedOption: string = "";
+  open = false;
+  filteredOptions: Array<string> = [];
+  currentHoverIndex = -1;
 
-    ngAfterViewInit() {
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    this.filteredOptions = this.options;
+    this.cdr.detectChanges();
+  }
+
+  focusIn() {
+    this.filterOnConnection();
+    this.open = true;
+  }
+
+  focusOut() {
+    setTimeout(() => this.open = false, 150);
+  }
+
+  keydown() {
+    return this.manualEntry;
+  }
+
+  keyup() {
+    if (!this.manualEntry) {
+      return;
+    }
+    this.filterOptions(this.selectedOption, this.labelProp);
+    this.cdr.detectChanges();
+  }
+
+  arrowDown() {
+    if (this.currentHoverIndex === (this.filteredOptions.length - 1)) {
+      return;
+    }
+    this.currentHoverIndex += 1;
+  }
+
+  arrowUp() {
+    if (this.currentHoverIndex === 0) {
+      return;
+    }
+    this.currentHoverIndex -= 1;
+  }
+
+  enterPressed() {
+    this.selectOption(this.filteredOptions[this.currentHoverIndex]);
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option[this.labelProp];
+    this.selectionChanged.emit(option);
+    this.open = false;
+  }
+
+  filterOptions(filterValue: string, filterProp: string) {
+    this.filterOnConnection();
+    this.filteredOptions = this.filteredOptions.filter((option) => option[filterProp].toLowerCase().indexOf(filterValue.toLowerCase()) !== -1);
+  }
+
+  filterOnConnection() {
+    if (this.connectTo && this.connectTo.selectedOption && this.connectToFilterProp) {
+      this.filteredOptions = this.options.filter((option) => {
+        let thisVal = option[this.connectToFilterProp];
+        return thisVal === "" || thisVal.toLowerCase().indexOf(this.connectTo.selectedOption.toLowerCase()) !== -1;
+      });
+    } else {
       this.filteredOptions = this.options;
-      this.cdr.detectChanges();
     }
+  }
 
-    focusIn() {
-      this.open = true;
-    }
-
-    focusOut() {
-      setTimeout(() => this.open = false, 250);
-    }
-
-    keydown() {
-      return this.manualEntry;
-    }
-
-    keyup() {
-      if (!this.manualEntry) {
-        return;
-      }
-      this.filteredOptions = this.options.filter((option) => option.toLowerCase().indexOf(this.selectedOption) !== -1);
-      console.log("filtered options length: " + this.filteredOptions.length);
-      this.cdr.detectChanges();
-    }
-
-    arrowDown() {
-      if (this.currentHoverIndex === (this.filteredOptions.length - 1)) {
-        return;
-      }
-      this.currentHoverIndex += 1;
-    }
-
-    arrowUp() {
-      if (this.currentHoverIndex === 0) {
-        return;
-      }
-      this.currentHoverIndex -= 1;
-    }
-
-    enterPressed() {
-      this.selectOption(this.filteredOptions[this.currentHoverIndex]);
-    }
-
-    selectOption(option: string) {
-      this.selectedOption = option;
-      this.open = false;
-    }
+  clearSelection() {
+    this.selectedOption = '';
+  }
 
 }
